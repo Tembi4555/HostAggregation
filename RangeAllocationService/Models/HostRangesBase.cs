f06ba258b2a45edf38e5ae28f7e1b47d65d79e9f
A_Grushev <A_Grushev@yamburg.gazprom.ru>
2023-12-06 17:11:56 +0500

    Изменения в HostRangeBase

--- RangeAllocationService/Models/HostRangesBase.cs
+++ RangeAllocationService/Models/HostRangesBase.cs
@@ -12,7 +12,6 @@ namespace HostAggregation.RangeAllocationService.Models
     public abstract class HostRangesBase
     {
         public string HostName { get; set; }
-        //public List<Range> Ranges { get; set; } = new List<Range>();
         public List<int?[]> Ranges { get; set; } = new List<int?[]>();
 
     }
